3b886832c5bcffc78fe878973b4e474cabd56ba5
Ralph Schmieder <rschmied@cisco.com>
2017-03-29 22:53:24 +0200

    adding a break condition for ctrl-c
    otherwise the thing will not stop spinning up topologies until the max is reached...
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- WORKDIR/batch.sh
+++ WORKDIR/batch.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-for i in $(seq 100); do { time virltester allnodes.yml ;}  >>main.log 2>&1 ; done
+MAX=100
+BREAK=0
+
+stopit() { BREAK=1; }
+
+
+trap stopit SIGINT
+
+for i in $(seq $MAX); do 
+    # { time virltester allnodes.yml ;} >>main.log 2>&1
+    { time virltester nxosv-9000-single-test.yml ;} >>main.log 2>&1
+    test $BREAK -eq 1 && break
+done
