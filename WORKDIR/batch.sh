ef6c0c8dffac7ebd311f13faf70160f7008e111c
Ralph Schmieder <rschmied@cisco.com>
2017-03-31 20:46:28 +0200

    more verbose
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- WORKDIR/batch.sh
+++ WORKDIR/batch.sh
@@ -1,15 +1,63 @@
 #!/bin/bash
 
-MAX=100
+CYCLES=100
 BREAK=0
+LOG="main.log"
 
-stopit() { BREAK=1; }
+stopit () { BREAK=1; }
 
+awk_cmd='
+function ptime(t) {
+  s=sprintf("%dm%ds", int(t/60), t%60)
+  return s
+}
+
+BEGIN {
+  hi=0;
+  lo=32768;
+  sum=0;
+  count=0;
+  failed=0;
+}
+
+/fail/ {
+  failed+=1;
+}
+
+/real/ {
+  split($2,a,"m");
+  time=int(a[1])*60+a[2];
+  sum+=time;
+  if (time>hi) hi=time;
+  if (time<lo) lo=time;
+  count+=1;
+}
+
+END {
+  if (count > 0) {
+    avg=sum/count;
+    printf("\rCycles: %d Failed: %d Low: %s High: %s Average: %s",
+      count, failed, ptime(lo), ptime(hi), ptime(avg));
+  }
+}
+'
+
+
+status () {
+    cat $LOG | awk "$awk_cmd" 
+}
 
 trap stopit SIGINT
 
-for i in $(seq $MAX); do 
-    # { time virltester allnodes.yml ;} >>main.log 2>&1
-    { time virltester nxosv-9000-single-test.yml ;} >>main.log 2>&1
+CMD=$*
+[ -z "$CMD" ] && CMD="allnodes.yml"
+
+echo $CMD
+
+for i in $(seq $CYCLES); do 
+    { time virltester $CMD ;} >>$LOG 2>&1
     test $BREAK -eq 1 && break
+    status
 done
+echo -e "\nDone!"
+
