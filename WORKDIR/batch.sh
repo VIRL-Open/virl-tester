96039c9c4915c26e2dbe717e7ea0e362026b8257
Ralph Schmieder <rschmied@cisco.com>
2017-04-06 09:14:55 +0200

    awk changes
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- WORKDIR/batch.sh
+++ WORKDIR/batch.sh
@@ -6,38 +6,51 @@ LOG="main.log"
 
 stopit () { BREAK=1; }
 
+
 awk_cmd='
 function ptime(t) {
-  s=sprintf("%dm%ds", int(t/60), t%60)
+  s = sprintf("%dm%02ds", int(t/60), t%60)
   return s
 }
 
 BEGIN {
-  hi=0;
-  lo=32768;
-  sum=0;
-  count=0;
-  failed=0;
+  hi = 0;
+  lo = 32768;
+  sum = 0;
+  cycles = 0;
+  failed = 0;
+  tests = 0
+  sims = 0
 }
 
-/fail/ {
-  failed+=1;
+/[[:digit:]]+ out of [[:digit:]]+/ {
+  tmp = match($0, /([[:digit:]]+) out of ([[:digit:]]+)/, a)
+  if(tmp) {
+    _all = int(a[2])
+    _fail = _all-int(a[1])
+    failed += _fail
+    tests += _all
+  }
+  cycles += 1;
 }
 
-/real/ {
-  split($2,a,"m");
-  time=int(a[1])*60+a[2];
-  sum+=time;
-  if (time>hi) hi=time;
-  if (time<lo) lo=time;
-  count+=1;
+/new thread.*\.virl/ {
+  sims += 1
+}
+
+/^real/ {
+  split($2, a, "m");
+  time = int(a[1]) * 60 + a[2];
+  sum += time;
+  if (time > hi) hi = time;
+  if (time < lo) lo = time;
 }
 
 END {
-  if (count > 0) {
-    avg=sum/count;
-    printf("\rCycles: %d Failed: %d Low: %s High: %s Average: %s",
-      count, failed, ptime(lo), ptime(hi), ptime(avg));
+  if (cycles > 0) {
+    avg = sum / cycles;
+    printf("Cycles: %d Sims: %d Tests: %d Failed: %d Low: %s High: %s Average: %s          \r",
+      cycles, sims, tests, failed, ptime(lo), ptime(hi), ptime(avg));
   }
 }
 '
@@ -52,7 +65,7 @@ trap stopit SIGINT
 CMD=$*
 [ -z "$CMD" ] && CMD="allnodes.yml"
 
-echo $CMD
+echo "Doing $CYCLES runs of $CMD..."
 
 for i in $(seq $CYCLES); do 
     { time virltester $CMD ;} >>$LOG 2>&1
@@ -60,4 +73,3 @@ for i in $(seq $CYCLES); do
     status
 done
 echo -e "\nDone!"
-
